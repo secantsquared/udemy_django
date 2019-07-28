b16c7bd338101ccd854e13f536df1c61a2d1433b
ryanboris <ryan.boris@gmail.com>
2019-07-28 12:06:57 -0400

    Remove linting errors

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,7 +7,10 @@ class ModelTests(TestCase):
         """Test creating new user with email is successful."""
         email = "test@test.com"
         password = "testpass12345"
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
@@ -26,7 +29,10 @@ class ModelTests(TestCase):
 
     def test_create_new_superuser(self):
         """Test creating a new superuser."""
-        user = get_user_model().objects.create_superuser("test@test.com", "test123")
+        user = get_user_model().objects.create_superuser(
+            "test@test.com",
+            "test123",
+        )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
