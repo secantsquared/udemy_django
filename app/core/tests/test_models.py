2d06d5764b9b32ab90a35508c0e5dd7f76e3e0d8
ryanboris <ryan.boris@gmail.com>
2019-07-28 12:02:25 -0400

    Create user management models

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email(self):
+        """Test creating new user with email is successful."""
+        email = "test@test.com"
+        password = "testpass12345"
+        user = get_user_model().objects.create_user(email=email, password=password)
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test for a normalized new user email."""
+        email = "test@TESTTEST.com"
+        user = get_user_model().objects.create_user(email, "test`123")
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, "test123")
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser."""
+        user = get_user_model().objects.create_superuser("test@test.com", "test123")
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
